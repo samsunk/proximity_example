8567e8b638e898f49373cd68bd0b307999c71c24
ShyamKhatiwada <samsunk03@gmail.com>
2022-02-06 14:44:39 +0545

    first commit

---
+++ android/app/src/main/kotlin/com/example/proximity_example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.proximity_example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart' as foundation;
+import 'dart:async';
+import 'package:proximity_sensor/proximity_sensor.dart';
+
+////////////////////////////////////////////////////////////////////////////////
+void main() {
+  runApp(MyApp());
+}
+
+////////////////////////////////////////////////////////////////////////////////
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+class _MyAppState extends State<MyApp> {
+  bool _isNear = false;
+  late StreamSubscription<dynamic> _streamSubscription;
+
+  @override
+  void initState() {
+    super.initState();
+    listenSensor();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _streamSubscription.cancel();
+  }
+
+  Future<void> listenSensor() async {
+    FlutterError.onError = (FlutterErrorDetails details) {
+      if (foundation.kDebugMode) {
+        FlutterError.dumpErrorToConsole(details);
+      }
+    };
+    _streamSubscription = ProximitySensor.events.listen((int event) {
+      setState(() {
+        _isNear = (event > 0) ? true : false;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Proximity Sensor Example'),
+        ),
+        body: Center(
+          child: Text('proximity sensor, is near ?  $_isNear\n'),
+        ),
+      ),
+    );
+  }
+}
